36e17dc86c0f3f4848df7ad071f16addcce61f4b
Kris Litman <kris.d.litman@gmail.com>
2022-05-25 21:14:59 -0400

    Refactored to meet json spec

--- lib/project_management_api_web/router.ex
+++ lib/project_management_api_web/router.ex
@@ -14,9 +14,14 @@ defmodule ProjectManagementApiWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :json_api do
+    plug :accepts, ["json-api"]
+    plug JaSerializer.Deserializer
+  end
+
   # Other scopes may use custom stacks.
   scope "/api/v1", ProjectManagementApiWeb do
-    pipe_through :api
+    pipe_through :json_api
 
     resources "/projects", ProjectController, only: [:index, :show]
     resources "/tasks", TaskController, only: [:index, :show]
--- lib/project_management_api_web/views/task_view.ex
+++ lib/project_management_api_web/views/task_view.ex
@@ -1,23 +1,6 @@
 defmodule ProjectManagementApiWeb.TaskView do
   use ProjectManagementApiWeb, :view
-  alias ProjectManagementApiWeb.TaskView
+  use JaSerializer.PhoenixView
 
-  def render("index.json", %{tasks: tasks}) do
-    %{data: render_many(tasks, TaskView, "task.json")}
-  end
-
-  def render("show.json", %{task: task}) do
-    %{data: render_one(task, TaskView, "task.json")}
-  end
-
-  def render("task.json", %{task: task}) do
-    %{
-      id: task.id,
-      name: task.name,
-      effort: task.effort,
-      notes: task.notes,
-      finished: task.finished,
-      project_id: task.project_id
-    }
-  end
+  attributes [:name, :effort, :notes, :finished, :project_id]
 end
