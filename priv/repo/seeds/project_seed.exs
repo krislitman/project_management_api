41102e8a025215c654fe07841022541994988161
Kris Litman <kris.d.litman@gmail.com>
2022-05-26 22:33:04 -0400

    Looping creation of projects

--- priv/repo/seeds/project_seed.exs
+++ priv/repo/seeds/project_seed.exs
@@ -1,18 +1,21 @@
 alias ProjectManagementApi.Repo
 alias ProjectManagementApi.Management.Project
 
+defmodule CreateProjects do
+    def create_projects_loop(n) when n <= 1 do
+       Repo.insert! %Project{
+            title: "My first Elixir Project",
+            description: "It is epic"
+        }
+    end
 
-Repo.insert! %Project{
-    title: "My first Elixir Project",
-    description: "It is epic"
-}
+    def create_projects_loop(n) do
+       Repo.insert! %Project{
+            title: "My first Elixir Project",
+            description: "It is epic"
+        }
+        create_projects_loop(msg, n - 1)
+    end
+ end
 
-Repo.insert! %Project{
-    title: "Turning Movement Count",
-    description: "Out in Aspen, CO"
-}
-
-Repo.insert! %Project{
-    title: "Parking Study",
-    description: "In Omaha, NE"
-}
+CreateProjects.create_projects_loop(10)
